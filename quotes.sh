1a18bed5c02d940840be1a2f81210690526988b4
Priyanka SP <85819116+PriyankaSP0910@users.noreply.github.com>
2023-03-14 12:24:28 +0530

    Create quotes.sh

---
+++ quotes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Purpose: Verifying Difference between quotation marks
+#Version: 1.0
+# START #
+VAR1=123456
+TEST=Priya
+
+# Double Quotes
+echo "Execute double quotes $VAR1 $TEST"
+
+# Single Quotes
+echo 'Excute Single Quotes $VAR1 $TEST'
+
+# Reverse Quotes
+echo "This Hostname is: `cal`"
+
+# END #
