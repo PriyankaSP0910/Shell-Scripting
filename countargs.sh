8d7c31ce8ed79a2e4e0a29998291d20cad6e1907
Priyanka SP <85819116+PriyankaSP0910@users.noreply.github.com>
2023-03-14 13:07:20 +0530

    Create countargs.sh

---
+++ countargs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# START #
+echo "Your current given parameters are $#"
+if [ $# -lt 1 ];then
+echo "Program Usage is './scriptname.sh' options"
+else
+echo "Program executed successfully"
+fi
+# END #
